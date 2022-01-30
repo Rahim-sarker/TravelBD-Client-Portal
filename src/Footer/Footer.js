9096b1c8adb57848f76608e361836c7e72e5d30c
Rahim-sarker <mhrahim08@gmail.com>
2022-01-30 13:09:27 +0600

    My Travel Project

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+
+        <nav class="navbar navbar-light bg-primary">
+            <div class="container-fluid">
+                <p className='text-primary'>Copyright © 2022 Travel.BD</p>
+            </div>
+        </nav>
+    );
+};
+
+export default Footer;
---
+++ src/firebase/config.firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqgQaC_Ys_fHEMCTNh8oRhllVkkKhjLzU",
+  authDomain: "travel-with-us-d3348.firebaseapp.com",
+  projectId: "travel-with-us-d3348",
+  storageBucket: "travel-with-us-d3348.appspot.com",
+  messagingSenderId: "355139085521",
+  appId: "1:355139085521:web:b39f965a6f6f53d75e51f0"
+};
+export default firebaseConfig;
